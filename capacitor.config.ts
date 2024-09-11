3ac762bf8b96811e10a7b883b63068566474f07a
Jonathan Leek <mrjonleek@gmail.com>
2024-09-10 20:42:50 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'open-authenticator',
+  webDir: 'www'
+};
+
+export default config;
