e516dacdb5820b26bc87fe39dc71b82bf2f39cc2
Jonathan Leek <mrjonleek@gmail.com>
2024-09-21 07:08:03 -0700

    Seeded an example account.

--- src/app/shared/services/account.service.ts
+++ src/app/shared/services/account.service.ts
@@ -1,4 +1,4 @@
-import { effect, Injectable, signal } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import { HOTP, TOTP, URI } from 'otpauth';
 
 @Injectable({
@@ -9,10 +9,8 @@ export class AccountService {
 
   constructor() {
     const seed = URI.parse(
-      'otpauth://totp/Tloop%3A?issuer=Floop&secret=I65VU7K5ZQL7WB4E&algorithm=SHA1&digits=6&period=30'
+      'otpauth://totp/user@example.com?issuer=Example%20ServiceS&secret=I65VU7K5ZQL7WB4E&algorithm=SHA1&digits=6&period=30'
     );
     this.accounts.set([<HOTP | TOTP>{ ...seed }]);
-
-    effect(() => console.log(this.accounts()));
   }
 }
