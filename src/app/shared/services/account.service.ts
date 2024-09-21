a7ac10a71ed958df0d7db3fac6704d34b1cb438c
Jonathan Leek <mrjonleek@gmail.com>
2024-09-21 07:06:33 -0700

    Added otpauth library and and types to the service.

--- src/app/shared/services/account.service.ts
+++ src/app/shared/services/account.service.ts
@@ -1,14 +1,18 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
+import { HOTP, TOTP, URI } from 'otpauth';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AccountService {
-  public accounts = signal([
-    {
-      issuer: 'GitHub',
-      label: 'mrjonleek@gmail.com',
-    },
-  ]);
-  constructor() {}
+  public accounts = signal<Array<HOTP | TOTP>>([]);
+
+  constructor() {
+    const seed = URI.parse(
+      'otpauth://totp/Tloop%3A?issuer=Floop&secret=I65VU7K5ZQL7WB4E&algorithm=SHA1&digits=6&period=30'
+    );
+    this.accounts.set([<HOTP | TOTP>{ ...seed }]);
+
+    effect(() => console.log(this.accounts()));
+  }
 }
