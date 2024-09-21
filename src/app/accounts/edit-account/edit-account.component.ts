5702a91bb869c83224aceab0e0f8f5d5bc43868d
Jonathan Leek <mrjonleek@gmail.com>
2024-09-21 10:49:40 -0700

    Edit account form.

--- src/app/accounts/edit-account/edit-account.component.ts
+++ src/app/accounts/edit-account/edit-account.component.ts
@@ -1,15 +1,19 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { NavController } from '@ionic/angular';
 
 import { UiService } from 'src/app/shared/services/ui.service';
 import { IonicBundleModule } from 'src/app/shared/ionic-bundle.module';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import {
   FormControl,
-  FormBuilder,
-  ReactiveFormsModule,
   Validators,
+  NonNullableFormBuilder,
 } from '@angular/forms';
+
+import { ActivatedRoute } from '@angular/router';
+import { AccountService } from 'src/app/shared/services/account.service';
+import { HOTP, TOTP } from 'otpauth';
 
 @Component({
   selector: 'app-edit-account',
@@ -18,34 +22,68 @@ import {
   imports: [IonicBundleModule, ReactiveFormsModule],
   standalone: true,
 })
-export class EditAccountComponent {
+export class EditAccountComponent implements OnInit {
+  protected fb = inject(NonNullableFormBuilder);
   protected nav = inject(NavController);
   protected ui = inject(UiService);
-  private fb = inject(FormBuilder);
 
-  editAccountForm = this.fb.group({
+  protected form = this.fb.group({
     issuer: new FormControl('', {
       nonNullable: true,
       validators: Validators.required,
     }),
-    label: new FormControl('', { nonNullable: true }),
+    label: new FormControl('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
   });
 
-  constructor() {}
+  private route = inject(ActivatedRoute);
+  private accounts = inject(AccountService).accounts;
+  private account = signal(<HOTP | TOTP>{});
+  private id!: number;
 
-  save() {}
+  constructor() {
+    this.form.valueChanges.subscribe((account) => {
+      if (this.form.valid) {
+        this.account.update(
+          (account) => <HOTP | TOTP>{ ...account, ...this.form.value }
+        );
+      }
+    });
+  }
 
+  ngOnInit(): void {
+    this.id = +this.route.snapshot.params['id'];
+    const account = computed(() => this.accounts()[this.id]);
+    this.form.patchValue({ issuer: account().issuer, label: account().label });
+  }
+
+  save() {
+    console.log(this.form.valid);
+    if (this.form.valid) {
+      this.accounts.update(
+        (accounts) => ((accounts[this.id] = this.account()), [...accounts])
+      );
+      this.form.markAsPristine();
+      this.nav.back();
+    }
+  }
+
+  /**
+   * Confirm data loss when canceling an
+   * incomplete form. The form will reset when
+   * canceled.
+   *
+   */
   async cancel() {
-    if (this.editAccountForm.dirty) {
-      const close = await this.ui.confirm({
-        message: { text: 'Are you sure? Unsasved changes will be lost.' },
-      });
-      if (close) {
-        this.editAccountForm.reset();
+    if (this.form.dirty) {
+      if (await this.ui.confirm()) {
+        this.form.reset();
         this.nav.back();
       }
     } else {
-      this.editAccountForm.reset();
+      this.form.reset();
       this.nav.back();
     }
   }
