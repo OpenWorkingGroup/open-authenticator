2271850de0c13b4f9346fc712bf5de230cdd00ba
Jonathan Leek <mrjonleek@gmail.com>
2024-09-21 07:08:38 -0700

    Corrected spelling mistake.

--- src/app/shared/services/account.service.ts
+++ src/app/shared/services/account.service.ts
@@ -9,7 +9,7 @@ export class AccountService {
 
   constructor() {
     const seed = URI.parse(
-      'otpauth://totp/user@example.com?issuer=Example%20ServiceS&secret=I65VU7K5ZQL7WB4E&algorithm=SHA1&digits=6&period=30'
+      'otpauth://totp/user@example.com?issuer=Example%20Service&secret=I65VU7K5ZQL7WB4E&algorithm=SHA1&digits=6&period=30'
     );
     this.accounts.set([<HOTP | TOTP>{ ...seed }]);
   }
